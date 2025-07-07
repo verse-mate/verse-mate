abe9bbec14496b473b0d312337736ce16da8c686
ThorSPB <sebastian.boghiu@gmail.com>
2025-07-07 15:44:45 +0300

    Fixed some visual bugs on firefox where the arrows are not properly shown because of the SVG masks.

--- packages/frontend-base/src/ui/Icons/chevronBackward.tsx
+++ packages/frontend-base/src/ui/Icons/chevronBackward.tsx
@@ -11,29 +11,11 @@ export const ChevronBackward = (props: SVGProps<SVGSVGElement>) => {
       aria-hidden="true"
       {...props}
     >
-      <mask
-        id="mask0_3882_1182"
-        maskUnits="userSpaceOnUse"
-        x="0"
-        y="0"
-        width="24"
-        height="24"
-      >
-        <rect
-          x="24"
-          y="24"
-          width="24"
-          height="24"
-          transform="rotate(-180 24 24)"
-          fill="#D9D9D9"
-        />
-      </mask>
-      <g mask="url(#mask0_3882_1182)">
-        <path
-          d="M15.9846 2.34631L17.4038 3.76556L9.16931 12.0001L17.4038 20.2346L15.9846 21.6538L6.33081 12.0001L15.9846 2.34631Z"
-          fill="white"
-        />
-      </g>
+      <path
+        d="M15.9846 2.34631L17.4038 3.76556L9.16931 12.0001L17.4038 20.2346L15.9846 21.6538L6.33081 12.0001L15.9846 2.34631Z"
+        fill="white"
+        style={{ shapeRendering: "geometricPrecision" }}
+      />
     </svg>
   );
 };
--- packages/frontend-base/src/ui/Icons/chevronForward.tsx
+++ packages/frontend-base/src/ui/Icons/chevronForward.tsx
@@ -11,22 +11,11 @@ export const ChevronForward = (props: SVGProps<SVGSVGElement>) => {
       aria-hidden="true"
       {...props}
     >
-      <mask
-        id="mask0_3612_5058"
-        maskUnits="userSpaceOnUse"
-        x="0"
-        y="0"
-        width="24"
-        height="24"
-      >
-        <rect width="24" height="24" fill="#D9D9D9" />
-      </mask>
-      <g mask="url(#mask0_3612_5058)">
-        <path
-          d="M8.01544 21.6537L6.59619 20.2344L14.8307 11.9999L6.59619 3.76544L8.01544 2.34619L17.6692 11.9999L8.01544 21.6537Z"
-          fill="white"
-        />
-      </g>
+      <path
+        d="M8.01544 21.6537L6.59619 20.2344L14.8307 11.9999L6.59619 3.76544L8.01544 2.34619L17.6692 11.9999L8.01544 21.6537Z"
+        fill="white"
+        style={{ shapeRendering: "geometricPrecision" }}
+      />
     </svg>
   );
 };
