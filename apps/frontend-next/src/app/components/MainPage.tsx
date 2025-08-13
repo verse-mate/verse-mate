f105fe64ad466f2cee8735aeac4d530d67c9eafd
Vijender <techyvijender@gmail.com>
2025-08-13 11:19:59 +0530

    Removed offline indicator component Removed related useNetwork status file

--- apps/frontend-next/src/app/components/MainPage.tsx
+++ apps/frontend-next/src/app/components/MainPage.tsx
@@ -8,15 +8,9 @@ const DynamicBrowserRouter = NextDynamic(
   { ssr: false },
 );
 
-const DynamicOfflineIndicator = NextDynamic(
-  () => import("frontend-base").then((mod) => mod.OfflineIndicator),
-  { ssr: false },
-);
-
 const MyMainPage = ({ children }: { children: ReactNode }) => {
   return (
     <MainPage.QueryProvider>
-      <DynamicOfflineIndicator position="top" showWhenOnline={true} />
       <DynamicBrowserRouter>{children}</DynamicBrowserRouter>
     </MainPage.QueryProvider>
   );
--- packages/frontend-base/src/ui/OfflineIndicator/index.ts
+++
@@ -1,1 +0,0 @@
-export { OfflineIndicator } from "./OfflineIndicator";
