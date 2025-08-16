275f847dbcf637a2ff3c4736d26ed2c89440d41b
ThorSPB <sebastian.boghiu@gmail.com>
2025-08-16 20:16:20 +0300

    fix: redundant check

--- packages/backend-base/src/bible/services/bible.service.ts
+++ packages/backend-base/src/bible/services/bible.service.ts
@@ -374,14 +374,13 @@ export class BibleService {
         g: book.genre_id,
         n: book.genre_name,
       },
-      chapters:
-        [
-          {
-            chapterNumber: chapter.chapter_number,
-            subtitles: subtitles,
-            verses: verses,
-          },
-        ] || [],
+      chapters: [
+        {
+          chapterNumber: chapter.chapter_number,
+          subtitles: subtitles,
+          verses: verses,
+        },
+      ], //the first array is always "truthy"
     };
   }
 
