0df6f4182839c34ed96ed97faa170e765e77a25b
ThorSPB <sebastian.boghiu@gmail.com>
2025-08-24 14:20:39 +0300

    chore: Moved all user prompts in a single file and removed duplicates.

--- apps/website/src/layouts/PageLayout.tsx
+++ apps/website/src/layouts/PageLayout.tsx
@@ -1,6 +1,6 @@
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { ReactNode } from "react";
+import Header from "@/components/Header";
+import type { ReactNode } from "react";
 
 interface PageLayoutProps {
   children: ReactNode;
