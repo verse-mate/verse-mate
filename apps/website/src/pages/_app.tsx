61ec4db853a653bee9d3d8f23c428ab007914d37
Sorin Zavelita <sorin.zavelita1976@gmail.com>
2025-09-03 17:18:11 +0300

    Fix the mobile viewport issue

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -9,9 +9,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Merriweather:wght@300;400;700;900&display=swap" 
-          rel="stylesheet" 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Merriweather:wght@300;400;700;900&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <Component {...pageProps} />
