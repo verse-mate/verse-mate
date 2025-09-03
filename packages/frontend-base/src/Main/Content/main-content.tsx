49c65f33e09a9c567f455eccf0c77c693b6e3e12
valentinBalasoiuCXT <valentin.balasoiu@cxtsoftware.com>
2025-09-03 20:04:52 +0300

    Fix/bookmarks mobile fix (#67)
    * added bookmarks mobile fix, bookmarks are now sorted and if we are on a bookmarked passage its corresponding bookmark in the list is highlighted.
    
    * fixed padding issue
    
    * mobile login menu needed fixed after main merged

--- packages/frontend-base/src/Main/Content/main-content.tsx
+++ packages/frontend-base/src/Main/Content/main-content.tsx
@@ -49,6 +49,7 @@ import { Settings } from "../../ui/Settings/Settings";
 import { Tabs } from "../../ui/Tabs";
 import { VerseGrid, useSelectedVerse } from "../../ui/VerseGrid/verse-grid";
 import { bibleVersions } from "../../utils/bible-versions";
+import { homeOptions } from "../../utils/home-options";
 import styles from "./main-content.module.css";
 
 export const MainContent = () => {
@@ -1274,10 +1275,27 @@ export const MainContent = () => {
                     setRightPanelContent={setRightPanelContent}
                   />
                 ) : session?.id ? (
-                  <ProfileButton
-                    link="/"
-                    setRightPanelContent={setRightPanelContent}
-                  />
+                  <>
+                    <ProfileButton
+                      link="/"
+                      setRightPanelContent={setRightPanelContent}
+                    />
+                    <div className={styles.menuOptions}>
+                      <Accordion.Root type="multiple">
+                        {homeOptions.map((option) => (
+                          <Accordion.Item key={option.name} value={option.name}>
+                            <Accordion.Trigger
+                              label={option.label}
+                              icon={option.icon}
+                            />
+                            <Accordion.Content>
+                              {option.content}
+                            </Accordion.Content>
+                          </Accordion.Item>
+                        ))}
+                      </Accordion.Root>
+                    </div>
+                  </>
                 ) : (
                   <>
                     {rightPanelContent === "login" && (
