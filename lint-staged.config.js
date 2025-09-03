337735e2a14718a5f00ed1ad7f4107b8cd2a74d5
ThorSPB <sebastian.boghiu@gmail.com>
2025-09-03 18:29:01 +0300

    chore: No longer ignoring bun run tsc and bun run lint on CI/CD also added lint:ci for CI/CD and on precommit added '--write'

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   "**/*": "biome format --write --no-errors-on-unmatched",
-  // "**/*.{ts,tsx}": () => "bunx tsc", need to fix elysia types
-  "**/*.{ts,tsx,parallel}": "biome check --write --no-errors-on-unmatched",
+  "**/*.{ts,tsx}": () => "bunx tsc",
+  "**/*.{ts,tsx,parallel}":
+    "bun --bun run --workspace apps/frontend-next lint:fix",
   "**/*.css": "stylelint --fix",
 };
