69a81a5aea259f576ebab69944288d6bd068a311
Augusto Chaves <chaves.augusto@gmail.com>
2025-09-06 15:21:41 -0300

    feat: use keyv client instead of redis

--- packages/backend-base/src/shared/redis-client.ts
+++ packages/backend-base/src/shared/redis-client.ts
@@ -1,18 +1,20 @@
+import KeyvRedis, { type RedisClientConnectionType } from "@keyv/redis";
+import Keyv from "keyv";
 import ms from "ms";
-import { type RedisClientType, createClient } from "redis";
 
-class RedisClient {
-  private client: RedisClientType;
+class CacheService {
+  private readonly keyv: Keyv;
+  private readonly redis: RedisClientConnectionType;
 
   constructor(redisUrl = "redis://localhost:6379") {
     const url = new URL(redisUrl);
     const password = url.password || undefined;
 
-    this.client = createClient({
+    const store = new KeyvRedis({
       url: redisUrl,
       ...(password ? { password } : {}),
       socket: {
-        reconnectStrategy: (retries) => {
+        reconnectStrategy: (retries: number) => {
           if (retries > 10) {
             console.log(
               `Max retry attempts reached. Please check your Redis server at ${redisUrl}`,
@@ -24,79 +26,56 @@ class RedisClient {
         },
       },
     });
+    this.redis = store.client;
+    this.keyv = new Keyv({ store });
 
-    this.client.on("error", (error) => {
+    this.keyv.on("error", (error) => {
       console.error("Redis Client Error", error);
     });
 
-    this.client.on("connect", () => {
+    this.keyv.on("connect", () => {
       console.log("Connected to Redis");
     });
 
-    this.client.on("reconnecting", () => {
+    this.keyv.on("reconnecting", () => {
       console.log("Reconnecting to Redis");
     });
 
-    this.client.on("end", () => {
+    this.keyv.on("end", () => {
       console.log("Disconnected from Redis");
     });
   }
 
-  async connect() {
-    if (!this.client.isOpen) {
-      try {
-        const timeout = new Promise((_, reject) =>
-          setTimeout(() => reject(new Error("Redis connection timeout")), 3000),
-        );
-        await Promise.race([this.client.connect(), timeout]);
-      } catch (error) {
-        console.error("Error connecting to Redis:", error);
-        throw error;
-      }
-    }
-  }
-
   async disconnect() {
-    if (this.client.isOpen) {
-      try {
-        await this.client.disconnect();
-      } catch (error) {
-        console.error("Error disconnecting from Redis:", error);
-      }
-    }
+    return this.keyv.disconnect();
   }
 
   async get<T>(key: string): Promise<T | null> {
-    await this.connect();
     try {
-      const value = await this.client.get(key);
-      if (value) {
-        return JSON.parse(value);
+      const value = await this.keyv.get(key);
+      if (!value) {
+        return null;
       }
 
-      return null;
+      return value as T;
     } catch (error) {
       console.error("Error getting key from Redis:", error);
       throw error;
     }
   }
 
-  async set(key: string, value: object, ttl: string): Promise<string | null> {
-    await this.connect();
+  async set(key: string, value: object, ttl: string): Promise<boolean> {
     try {
-      return await this.client.set(key, JSON.stringify(value), {
-        PX: ms(ttl),
-      });
+      return await this.keyv.set(key, value, ms(ttl));
     } catch (error) {
       console.error("Error setting value in Redis:", error);
       throw error;
     }
   }
 
-  async delete(key: string): Promise<number> {
-    await this.connect();
+  async delete(key: string): Promise<boolean> {
     try {
-      return await this.client.del(key);
+      return await this.keyv.delete(key);
     } catch (error) {
       console.error("Error deleting key from Redis:", error);
       throw error;
@@ -107,5 +86,5 @@ class RedisClient {
 const redisUrl = process.env.REDIS_URL ?? "redis://:abcd1234@localhost:6379/0";
 
 // Export both the class and instance with explicit typing
-export { RedisClient };
-export default new RedisClient(redisUrl);
+export { CacheService };
+export default new CacheService(redisUrl);
