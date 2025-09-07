f6fa2c11776a1bcfde794848e56fb1298284c8ad
Augusto Chaves <chaves.augusto@gmail.com>
2025-09-07 17:29:26 -0300

    chore: disable other languages

--- packages/frontend-base/src/utils/bible-versions.ts
+++ packages/frontend-base/src/utils/bible-versions.ts
@@ -4,28 +4,28 @@ export const bibleVersions = [
     value: "New American Standard Bible 1995 (NASB1995)",
     language: "en",
   },
-  { key: "CUV23", value: "БІБЛІЯ Сучасний переклад (CUV23)", language: "uk" },
-  { key: "ELBBK", value: "Elberfelder Übersetzung (ELBBK)", language: "de" },
-  {
-    key: "FRC97",
-    value: "La Bible en français courant (FRC97)",
-    language: "fr",
-  },
-  { key: "LTT18", value: "Bíblia de Estudo LTT 2018 (LTT18)", language: "pt" },
-  { key: "NR94", value: "Nuova Riveduta 1994 (NR94)", language: "it" },
-  {
-    key: "NRT23",
-    value: "Новый русский перевод (НРП), 2023 (NRT23)",
-    language: "ru",
-  },
-  {
-    key: "RVA15",
-    value: "Reina Valera Actualizada, 2015 (RVA15)",
-    language: "es",
-  },
-  {
-    key: "VDC2016",
-    value: "Biblia sau Sfânta Scriptură cu Trimiteri (VDC2016)",
-    language: "ro",
-  },
+  // { key: "CUV23", value: "БІБЛІЯ Сучасний переклад (CUV23)", language: "uk" },
+  // { key: "ELBBK", value: "Elberfelder Übersetzung (ELBBK)", language: "de" },
+  // {
+  //   key: "FRC97",
+  //   value: "La Bible en français courant (FRC97)",
+  //   language: "fr",
+  // },
+  // { key: "LTT18", value: "Bíblia de Estudo LTT 2018 (LTT18)", language: "pt" },
+  // { key: "NR94", value: "Nuova Riveduta 1994 (NR94)", language: "it" },
+  // {
+  //   key: "NRT23",
+  //   value: "Новый русский перевод (НРП), 2023 (NRT23)",
+  //   language: "ru",
+  // },
+  // {
+  //   key: "RVA15",
+  //   value: "Reina Valera Actualizada, 2015 (RVA15)",
+  //   language: "es",
+  // },
+  // {
+  //   key: "VDC2016",
+  //   value: "Biblia sau Sfânta Scriptură cu Trimiteri (VDC2016)",
+  //   language: "ro",
+  // },
 ];
