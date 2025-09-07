525628b8bb021618b3c666d3fba6bb71d146cc14
ThorSPB <sebastian.boghiu@gmail.com>
2025-09-08 01:08:22 +0300

    fix: push url only on chapter/book change

--- packages/frontend-base/src/hooks/useSearchParams.ts
+++ packages/frontend-base/src/hooks/useSearchParams.ts
@@ -34,7 +34,11 @@ export const useSaveSearchParams = () => {
 
     const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
 
-    window.history.pushState({}, "", newUrl);
+    if (bookId || verseId) {
+      window.history.pushState({}, "", newUrl);
+    } else {
+      window.history.replaceState({}, "", newUrl);
+    }
   };
 
   const saveBibleVersionOnURL = (bibleVersion: string) => {
