cf6250ebf0d88157d5f07cd100fc6e3a6933bdd5
Augusto Chaves <chaves.augusto@gmail.com>
2025-10-22 21:44:23 -0300

    ci: enable seeder

--- packages/database/src/seeder.ts
+++ packages/database/src/seeder.ts
@@ -4,11 +4,6 @@ import { main as runSeedScript } from "../../backend-base/src/bible/seed";
 import type Database from "./models/Database";
 
 async function runSeed() {
-  if (process.env.ENVIRONMENT === "test") {
-    console.log("Skipping seed script in test environment");
-    return;
-  }
-
   const db = new Kysely<Database>({
     dialect: new PostgresDialect({
       pool: new Pool({
