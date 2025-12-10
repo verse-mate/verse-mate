ac4deeb257bce6c519c503f9aca8af53ac6df7fb
Augusto Chaves <chaves.augusto@gmail.com>
2025-12-10 16:19:32 -0300

    fix: lint (#131)
    * chore: trigger CI re-run [patch]
    
    * fix: lint

--- packages/backend-base/index.ts
+++ packages/backend-base/index.ts
@@ -4,10 +4,10 @@ import authPlugin from "./src/auth/auth.plugin";
 import type { AuthPlugin } from "./src/auth/auth.plugin";
 import biblePlugin from "./src/bible/bible.plugin";
 import type { BiblePlugin } from "./src/bible/bible.plugin";
-import userPlugin from "./src/user/user.plugin";
-import type { UserPlugin } from "./src/user/user.plugin";
 import healthCheckPlugin from "./src/healthcheck/healthcheck.plugin";
 import type { HealthCheckPlugin } from "./src/healthcheck/healthcheck.plugin";
+import userPlugin from "./src/user/user.plugin";
+import type { UserPlugin } from "./src/user/user.plugin";
 
 import topicPlugin from "./src/topics/topic.plugin";
 import type { TopicPlugin } from "./src/topics/topic.plugin";
